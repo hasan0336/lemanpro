e54c34e54a74a17097e2e70ffcd857139f7459b8
Hasan <hasan.mehdi@technado.ltd>
2019-11-06 17:25:06 +0500

    lib url

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -14,6 +14,7 @@ use App\Mail\UserNotification;
 use App\Mail\NewOtp;
 use App\Profile;
 use Hash;
+use URL;
 // use Illuminate\Support\Facades\Mail;
 class AuthController extends ResponseController
 {
