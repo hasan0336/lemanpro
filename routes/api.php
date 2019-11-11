43cbc7a1a5b8df4eb4662964ae6807b21c3775c0
Hasan <hasan.mehdi@technado.ltd>
2019-11-11 17:39:51 +0500

    logout api

--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,9 @@ Route::group(['middleware' => ['auth:api','client.credentials']], function(){
 	Route::post('create_news','API\NewsController@create_news');
 	Route::post('edit_news','API\NewsController@edit_news');
 
+	
+	Route::post('logout','API\AuthController@logout');
+
 });
 // Route::middleware(['auth:api','client.credentials'])->get('/user', function (Request $request) {
 
