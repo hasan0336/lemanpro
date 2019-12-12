2eb4a725afc5f774e235b592ee61f83976808057
Hasan <hasan.mehdi@technado.ltd>
2019-12-12 16:14:48 +0500

    search player and tryouts

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -16,7 +16,7 @@ class SearchController extends ResponseController
 {
     public function search_tryout(Request $request)
     {
-    	$longitude = $request->longtude;
+    	$longitude = $request->longitude;
     	$latitude = $request->latitude;
     	$miles = $request->miles;
     	// $gender = $request->gender;
@@ -34,11 +34,11 @@ class SearchController extends ResponseController
     	}
     	elseif($search_team_name && $latitude && $longitude)
     	{
-    		$results = DB::select(DB::raw('SELECT team_id,tryouts.id as tryout_id,team_name,costoftryout,dateoftryout,timeoftryout,latitude,longitude,street, ( 3959 * acos( cos( radians(' . $latitude . ') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude .') ) * sin( radians(latitude) ) ) ) AS distance FROM tryouts join profiles on profiles.user_id = tryouts.team_id where profiles.team_name LIKE "%'.$search_team_name.'%" HAVING distance < ' . $miles . ' ORDER BY distance') );
+    		$results = DB::select(DB::raw('SELECT team_id,tryouts.id as tryout_id,team_name,costoftryout,dateoftryout,timeoftryout,tryouts.latitude as latitude,tryouts.longitude as longitude,street, ( 3959 * acos( cos( radians(' . $latitude . ') ) * cos( radians( tryouts.latitude ) ) * cos( radians( tryouts.longitude ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude .') ) * sin( radians(tryouts.latitude) ) ) ) AS distance FROM tryouts join profiles on profiles.user_id = tryouts.team_id where profiles.team_name LIKE "%'.$search_team_name.'%" HAVING distance < ' . $miles . ' ORDER BY distance') );
     	}
     	else
     	{
-    		$results = DB::select(DB::raw('SELECT team_id,tryouts.id as tryout_id,team_name,costoftryout,dateoftryout,timeoftryout,latitude,longitude,street, ( 3959 * acos( cos( radians(' . $latitude . ') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude .') ) * sin( radians(latitude) ) ) ) AS distance FROM tryouts join profiles on profiles.user_id = tryouts.team_id HAVING distance < ' . $miles . ' ORDER BY distance') );
+    		$results = DB::select(DB::raw('SELECT team_id,tryouts.id as tryout_id,team_name,costoftryout,dateoftryout,timeoftryout,tryouts.latitude as latitude,tryouts.longitude as longitude,street, ( 3959 * acos( cos( radians(' . $latitude . ') ) * cos( radians( tryouts.latitude ) ) * cos( radians( tryouts.longitude ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude .') ) * sin( radians(tryouts.latitude) ) ) ) AS distance FROM tryouts join profiles on profiles.user_id = tryouts.team_id HAVING distance < ' . $miles . ' ORDER BY distance') );
 
     	}
         $success['status'] = "1";
@@ -49,16 +49,17 @@ class SearchController extends ResponseController
 
     public function search_player(Request $request)
     {
-        $longitude = $request->longtude;
+        $longitude = $request->longitude;
         $latitude = $request->latitude;
         $miles = $request->miles;
         $gender = $request->gender;
         $age = $request->age;
-        $bd  = date('Y', strtotime($age . ' years ago'));
-        dd($bd);
+        $dob  = date('Y', strtotime($age . ' years ago'));
+        // dd($request->all());
+        // dd($bd);
         // $longitude = "";
         // $latitude = ""; 
-        // $miles = 150;
+            // $miles = 150;
         // $gender = "male";
         // $age = 24;
 
@@ -68,16 +69,21 @@ class SearchController extends ResponseController
         }
         elseif($age && $gender == null && $latitude == null && $longitude == null)
         {
-            $results = Profile::select('user_id','first_name','last_name',DB::raw("CONCAT('".URL::to('/images/profile_images/')."/',image) AS imageurl"))->whereyear('dob','=',$age})->get();
+            $results = Profile::select('user_id','first_name','last_name',DB::raw("CONCAT('".URL::to('/images/profile_images/')."/',image) AS imageurl"))->whereyear('dob','=',$dob)->get();
         }
         elseif($gender && $latitude && $longitude && $age)
         {
-            $results = DB::select(DB::raw('SELECT id,user_id.id as player_id,first_name,last_name,latitude,longitude,image, ( 3959 * acos( cos( radians(' . $latitude . ') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude .') ) * sin( radians(latitude) ) ) ) AS distance FROM profiles where profiles.gender LIKE "%'.$gender.'%" && where profiles.dob LIKE "%'.$age.'%" HAVING distance < ' . $miles . ' ORDER BY distance') );
+            // dd(33);
+            $results = DB::select(DB::raw('SELECT id,user_id as player_id,first_name,last_name,latitude,longitude,image, ( 3959 * acos( cos( radians('.$latitude.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('.$longitude.') ) + sin( radians('.$latitude.') ) * sin( radians(latitude) ) ) ) AS distance FROM profiles where profiles.gender LIKE "%'.$gender.'%" AND year(profiles.dob)  = '.$dob.' HAVING distance < ' . $miles . ' ORDER BY distance') );
+        }
+        elseif($gender && $age)
+        {
+            
+            $results = Profile::select('user_id','first_name','last_name',DB::raw("CONCAT('".URL::to('/images/profile_images/')."/',image) AS imageurl"))->where('gender', 'LIKE', "%{$gender}%")->whereyear('dob','=',$dob)->get();
         }
         else
         {
-            $results = DB::select(DB::raw('SELECT id,user_id.id as player_id,first_name,last_name,latitude,longitude,image, ( 3959 * acos( cos( radians(' . $latitude . ') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude .') ) * sin( radians(latitude) ) ) ) AS distance FROM profiles HAVING distance < ' . $miles . ' ORDER BY distance') );
-
+            $results = DB::select(DB::raw('SELECT id,user_id as player_id,first_name,last_name,latitude,longitude,image, ( 3959 * acos( cos( radians('.$latitude.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('.$longitude.') ) + sin( radians('.$latitude.') ) * sin( radians(latitude) ) ) ) AS distance FROM profiles HAVING distance < 150 ORDER BY distance') );
         }
         $success['status'] = "1";
         $success['message'] = " User";
